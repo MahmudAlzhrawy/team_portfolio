92670fdf131f59968493f7d20c4f2881f77793b6
unknown <mahmudalzhrawy@gmail.com>
2025-04-25 23:44:19 +0300

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <h1 className="text-chestnut-dark text-5xl md:text-6xl font-alexandria tracking-normal leading-tight">
         Transforming your idea into a successful digital product
       </h1>
-      <p className="text-chestnut-light mt-4 text-lg md:text-xl max-w-2xl mx-auto">
+      <p className="text-chestnut-light font-alexandria mt-4 text-lg md:text-xl max-w-2xl mx-auto">
         At Codeverse, we excel in designing and developing websites and mobile applications with high quality and unique user experiences.
       </p>
       <button className="mt-10 px-6 py-4 w-full sm:w-1/2 rounded-2xl text-xl font-mono bg-chestnut-rosewood text-cream hover:bg-chestnut-mahogany transition duration-500">
